76cd78d4a8f51d84b1bc17396aabbf8aae0832ac
Francine Altman <francinekline@gmail.com>
2025-10-23 19:11:32 -0400

    style: update media queries

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import MealList from "./components/MealList";
 
 export default function App() {
   return (
-    <div style={{ maxWidth: 600, margin: "0 auto", padding: 20 }}>
+    <div className="app-container">
       <h1>Yummy Canadian Recipes</h1>
       <MealList />
     </div>
--- src/components/MealItem.tsx
+++ src/components/MealItem.tsx
@@ -18,12 +18,14 @@ export default function MealItem({ meal }: { meal: Meal }) {
   return (
     <li className="meal-item">
       <h2>{meal.strMeal}</h2>
+      <div className="pic-and-instructions">
       <img
         src={meal.strMealThumb}
         alt={meal.strMeal}
         className="meal-image"
       />
-      <p>{meal.strInstructions}</p>
+      <p className="meal-instructions">{meal.strInstructions}</p>
+      </div>
       <h3>Ingredients</h3>
       <ul className="ingredients-list">
         {ingredients.map((text, idx) => (
